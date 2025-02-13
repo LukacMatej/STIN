e51cd044baa45c848b7601ae11e141e2ca807eaf
Matej Lukac <matej.lukac@tul.cz>
2025-02-13 13:25:15 +0100

    flask init s indexem

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,23 @@
-import django
+import flask
+import argparse
+import waitress
 
-def main():
-    print("Hello, World!")
+app = flask.Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return 'Hello, Flask!'
+
+def argParse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='A simple Flask application.')
+    parser.add_argument('--debug', action='store_true', help='Run the application in debug mode.')
+    args: argparse.Namespace = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
-    main()
+    args: argparse.Namespace = argParse()
+    if args.debug:
+        app.run(debug=True)
+    else:
+        waitress.serve(app, port=5000)
