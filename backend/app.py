4dbdd71ac797bcb19ffbe212dc644c039143f838
Matej Lukac <matej.lukac@tul.cz>
2025-02-25 16:33:08 +0100

    init flask

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,21 @@
 import flask
 import argparse
 import waitress
+from app.stock.service import stock_service as ss 
+from app.stock.model import stock_model as sm
 
 app = flask.Flask(__name__)
 
 @app.route('/')
-
 def index():
     return 'Hello, Flask!'
+
+@app.route('/getStock')
+def getStock():
+    stock_client = ss.Finnhub()
+    stock = stock_client.getCompanyEarning()
+    return flask.jsonify(stock)
+    
 
 def argParse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='A simple Flask application.')
