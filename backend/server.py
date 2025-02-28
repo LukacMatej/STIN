94ce2abd3b90ff4238d73ec19ad591bc72315cca
Matej Lukac <matej.lukac@tul.cz>
2025-02-28 07:51:50 +0100

    Uprava serevr

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,6 @@ from google.genai.types import GenerateContentResponse
 import waitress
 import sys
 import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.stock.service import stock_service as ss
 from app.genai.service import genai_service as gs
