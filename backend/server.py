b4e3e4c7fab736ffc9ccd6907e4278723a0dfc7f
Matej Lukac <matej.lukac@tul.cz>
2025-03-07 08:14:07 +0100

    New argparser

--- backend/server.py
+++ backend/server.py
@@ -66,14 +66,24 @@ def register():
     redirect(url_for('login'))
     return 200, 'Registration successful'
 
-def argParse() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='A simple Flask application.')
-    parser.add_argument('--debug', action='store_true', help='Run the application in debug mode.')
-    args: argparse.Namespace = parser.parse_args()
-    return args
+def parser_init() -> argparse.ArgumentParser:
+    """
+    Initialize the argument parser for the server.
+
+    Returns:
+        argparse.ArgumentParser: The argument parser object.
+    """
+    argparser = argparse.ArgumentParser(description="Turn on/off production.")
+    argparser.add_argument(
+        "-d", "--development", help="Turn on development server", action="store_true"
+    )
+    argparser.add_argument(
+        "-debug", "--debug", help="Turn on debug mode", action="store_true"
+    )
+    return argparser
 
 if __name__ == "__main__":
-    parser: argparse.ArgumentParser = argParse()
+    parser: argparse.ArgumentParser = parser_init()
     args: argparse.Namespace = parser.parse_args()
     docker_ip: str = os.environ.get("LISTEN_ADDRESS")
     docker_port: str = os.environ.get("HTTP_PORT")
