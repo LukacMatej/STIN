89e3c04c5ed0897ed91759b359e5f0e2853567a1
Matej Lukac <matej.lukac@tul.cz>
2025-03-07 07:32:03 +0100

    Rest endpoint registrace a login

--- backend/app/auth/sign_up/model/sign_up_model.py
+++ backend/app/auth/sign_up/model/sign_up_model.py
@@ -1,3 +1,5 @@
+import random
+
 class SignUpModel:
     def __init__(self, email: str, password: str, first_name: str, last_name: str, second_password: str) -> None:
         self.email: str = email
@@ -5,6 +7,16 @@ class SignUpModel:
         self.first_name: str = first_name
         self.last_name:str  = last_name
         self.second_password: str = second_password
+        self.id: int = self.generateId()
         
     def __str__(self):
         return f"{self.email} {self.password} {self.first_name} {self.last_name} {self.second_password}"
+    
+    def generateId(self) -> int:
+        # Ensure the ID is unique by checking against existing IDs
+        existing_ids = set()  # This should be replaced with actual storage of IDs
+        new_id = random.randint(1000, 9999)
+        while new_id in existing_ids:
+            new_id = random.randint(1000, 9999)
+            existing_ids.add(new_id)
+        return new_id
--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,16 @@
-import flask
+from flask import Flask, redirect, url_for, session, jsonify, request
 import argparse
 from google.genai.types import GenerateContentResponse
 import waitress
-import sys
 import os
-import json
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.stock.service import stock_service as ss
 from app.genai.service import genai_service as gs
-import time
-from google.genai.errors import ClientError
+from app.auth.sign_up.model import sign_up_model as sum
+from app.auth.sign_in.model import sign_in_model as sim
+from app.auth.service import auth_service
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -20,8 +18,8 @@ def index():
 
 @app.route('/getStock')
 def getStock():
-    finnhub_api_key = os.environ.get('FINNHUB_API_KEY','cuuudb9r01qpi6rs7eg0cuuudb9r01qpi6rs7egg')
-    genai_api_key = os.environ.get('GEN_AI_KEY','AIzaSyCXhf0yGZX_l1VSkCLoYfL89yNaI-ARbrg')
+    finnhub_api_key: str = os.environ.get('FINNHUB_API_KEY')
+    genai_api_key: str = os.environ.get('GEN_AI_KEY')
     
     genai_client = gs.genaiClient(genai_api_key)
     client = ss.FinnhubClient(finnhub_api_key)
@@ -33,8 +31,43 @@ def getStock():
     evaluated_stocks: list[str] = str(response.text).split('\n')
     print(evaluated_stocks)
     print(len(evaluated_stocks))
-    return flask.jsonify(evaluated_stocks)
+    return jsonify(evaluated_stocks)
     
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))    
+
+@app.route('/login', methods=['POST'])
+def login():
+    email: str = request.json.get('email')
+    password: str = request.json.get('password')
+    sign_in_model = sim.SignInModel(email, password)
+    validated: bool = auth_service.validateLogin(sign_in_model)
+    if validated:
+        session['loggedin'] = True
+        session['id'] = sign_in_model['id']
+        session['username'] = sign_in_model['username']
+        msg = 'Logged in successfully !'
+        redirect(url_for(''))
+        return msg
+    else:
+        msg = 'Incorrect username / password !'
+    return msg
+
+@app.route('/register', methods=['POST'])
+def register():
+    email: str = request.json.get('email')
+    password: str = request.json.get('password')
+    first_name: str = request.json.get('first_name')
+    last_name: str = request.json.get('last_name')
+    second_password: str = request.json.get('second_password')
+    sign_up_model = sum.SignUpModel(email, password, first_name, last_name, second_password)
+    auth_service.saveRegistrationJson(sign_up_model)
+    redirect(url_for('login'))
+    return 'Registration successful !'
 
 def argParse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='A simple Flask application.')
